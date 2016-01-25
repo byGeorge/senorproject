64cb2ecf9778071859f933559bcfcc064a752d5d
george@aprillemae.com <george@aprillemae.com>
2016-01-24 21:08:07 -0700

    FINALLY CONNECTING TO DB. Now all I gotta do is make it pretty

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,46 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160122080955) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "races", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "dwarf_names", primary_key: "NID", force: :cascade do |t|
+    t.string "name", limit: 30
   end
+
+  add_index "dwarf_names", ["name"], name: "dwarf_names_name_key", unique: true, using: :btree
+
+  create_table "elf_names", primary_key: "NID", force: :cascade do |t|
+    t.string "name", limit: 30
+  end
+
+  add_index "elf_names", ["name"], name: "elf_names_name_key", unique: true, using: :btree
+
+  create_table "elf_syllables", primary_key: "SID", force: :cascade do |t|
+    t.string "syllable", limit: 3, null: false
+  end
+
+  add_index "elf_syllables", ["syllable"], name: "elf_syllables_syllable_key", unique: true, using: :btree
+
+  create_table "human_names", primary_key: "NID", force: :cascade do |t|
+    t.string "first_name", limit: 30
+    t.string "last_name",  limit: 30
+    t.string "nickname",   limit: 30
+    t.string "Gender",     limit: 1
+  end
+
+  add_index "human_names", ["first_name", "last_name", "nickname"], name: "human_names_first_name_last_name_nickname_key", unique: true, using: :btree
+
+  create_table "races", primary_key: "rid", force: :cascade do |t|
+    t.string "name", limit: 10
+  end
+
+  create_table "users", primary_key: "uid", force: :cascade do |t|
+    t.string "name", limit: 20
+  end
+
+  add_index "users", ["name"], name: "users_name_key", unique: true, using: :btree
 
 end
