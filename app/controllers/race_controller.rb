82d5c0566584061fd81ce9ff881f37241ac252f1
george@aprillemae.com <george@aprillemae.com>
2016-01-24 22:23:38 -0700

    Selector box now selectinating.

--- app/controllers/race_controller.rb
+++ app/controllers/race_controller.rb
@@ -1,7 +1,10 @@
 class RaceController < ApplicationController
 	def list
-		puts(Race.all)
 		puts("RaceController output")
 		@races = Race.all
+	end
+
+	def print_name(object)
+		object.name
 	end
 end
--- app/models/race.rb
+++ app/models/race.rb
@@ -1,7 +1,8 @@
 
 class Race < ActiveRecord::Base
 	self.primary_key = "RID"
-	puts(Race.all.count)
-	puts("RaceModel working. I hope.") 
 	@races = Race.all
+	def print_name
+		name
+	end
 end
